2ea9b9d259a794e3bbdec8295ba7058aa236b5fb
Login_vano1234 <116017080+VanoLogin@users.noreply.github.com>
2024-06-16 20:16:05 +0300

    update_16.06

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import ContactList from "./components/ContactList/ContactList.jsx";
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import { selectLoading, selectError } from "./redux/contactsSlice.js";
+import { fetchContacts } from "./interfaces/interfaces.js";
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const isError = useSelector(selectError);
+  const isLoading = useSelector(selectLoading);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error...</p>}
+        <ContactList />
+      </div>
+    </>
+  );
+}
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./styles.module.css";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
+
+export default function SearchBox() {
+  const dispatch = useDispatch();
+  const searchValue = useSelector(selectNameFilter);
+
+  const handleChange = (event) => {
+    dispatch(changeFilter(event.target.value));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.span}>Find contacts by name</span>
+      <input type="text" value={searchValue} onChange={handleChange} />
+    </div>
+  );
+}
